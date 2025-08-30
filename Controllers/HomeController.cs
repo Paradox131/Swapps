f6cc1169d0b6fbabcfbfd893f52291d284e9add0
Sam <samuelakisanmi7@gmail.com>
2025-08-30 20:45:28 +0100

    Login/Logout,register verifyEmail  and ChangePassword function are all working problems with CSS and backgroud image though

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Swapps.Models;
 
@@ -17,7 +18,7 @@ namespace Swapps.Controllers
         {
             return View();
         }
-
+        [Authorize]
         public IActionResult Privacy()
         {
             return View();
