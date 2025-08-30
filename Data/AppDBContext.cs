c4333dff0d767812d029b8dea69fa31f876ace7a
Sam <samuelakisanmi7@gmail.com>
2025-08-30 17:51:50 +0100

    reworks and installs

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -1,9 +1,10 @@
-﻿using Swapps.Models;
-using System.Security.Principal;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Swapps.Models;
 
 namespace Swapps.Data
 {
-    public class AppDBContext : IdentityDBContext<Users>
+    public class AppDbContext : IdentityDbContext<Users>
     {
         public AppDbContext(DbContextOptions options) : base(options)
         {
--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Swapps.Data;
-
+using Swapps.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+//builder.Services.AddDbContext<AppDBContext>(options =>
+//options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
