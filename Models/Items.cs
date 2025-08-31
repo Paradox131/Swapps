cfc9d68fd14ae8e27fa8096b2379db2473d1cb27
Sam <samuelakisanmi7@gmail.com>
2025-08-31 20:04:19 +0100

    minor changes still incomplete

--- Models/Items.cs
+++ Models/Items.cs
@@ -1,6 +1,20 @@
-﻿namespace Swapps.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Swapps.Models
 {
     public class Items
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Category { get; set; } // Clothes, Toys, Skills, Food
+
+        public string ImagePath { get; set; }
+
+        public bool IsApproved { get; set; } = false;
     }
 }
